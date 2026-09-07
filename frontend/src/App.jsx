3e936ddcc2df9fc8387fb00f8662aef1afd8c00d
Maninder Solitaire <manindersolitaire@gmail.com>
2026-09-07 12:31:40 +0530

    Batch 2026 QuickBlog Day 1(Minor)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
-import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
