9610f34723cd5bb1bfaa5ba5f41398c9fd65f5f9
Maninder Solitaire <manindersolitaire@gmail.com>
2026-09-09 11:24:31 +0530

    Batch 2026 QuickBlog Day 3(Minor)

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
+      <div className=''>
+
+      </div>
+      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2026 &copy; QuickBlog Solitaire - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
