e15e3d1124b03fd02bf96a935fcc7ff98ec3a88c
Maninder Solitaire <manindersolitaire@gmail.com>
2026-09-14 11:14:54 +0530

    Batch 2026 QuickBlog Backend day 1

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+import dns from 'dns'
+
+dns.setServers(['0.0.0.0', '8.8.8.8'])
+export const connectDB = async()=>{
+    try {
+        mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { connectDB } from './config/db.js'
+import adminRouter from './routes/adminRoutes.js'
+
+const app = express()
+
+await connectDB()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req,res)=>{
+    res.send("API is working...")
+})
+app.use('/api/admin', adminRouter)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`)
+})
+
+
